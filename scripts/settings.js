757c53b5c3debdcf2be19e7880389bc222167668
bbheemara <bbheemaraya944@gmail.com>
2025-09-22 22:47:22 +0530

    Updated ui with required elements to test, added js logic to ui for dynamicness

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,6 +1,8 @@
 const wallpaper_id = document.getElementById("Wallpaper-options")
 const type_a_genre = document.getElementById("typeagenre")
 const custom = document.getElementById("custom")
+const quote_id = document.getElementById("quote-options")
+const quote_input = document.getElementById("quote_custom_div")
 
 wallpaper_id.addEventListener("change", () => {
  type_a_genre.classList.add("hiddenc");
@@ -16,3 +18,19 @@ custom.classList.add("hiddenc");
  custom.classList.add("visible");
 }
 });
+
+quote_id.addEventListener("change", () =>
+{
+    quote_input.classList.add("hiddenc");
+    quote_input.classList.remove("visible")
+
+    if (quote_id.value === "Custom"){
+        quote_input.classList.remove("hiddenc")
+        quote_input.classList.add("visible")
+    }
+    else{
+         custom.classList.remove("visible");
+ custom.classList.add("hiddenc");
+    }
+
+})
